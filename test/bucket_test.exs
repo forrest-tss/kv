42d7a1a6b1991ea11c6c2e83afd1d0fd22b12f4f
Forrest Hayes <forrest.hayes@tssands.com>
2022-04-16 12:04:18 -0500

    Add a basic key, value store using Agent

---
+++ test/bucket_test.exs
@@ -0,0 +1,23 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "keys can be deleted; returns value if key exists",
+  %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 2)
+
+    assert KV.Bucket.delete(bucket, "eggs") == 2
+    assert KV.Bucket.get(bucket, "eggs") == nil
+  end
+end
